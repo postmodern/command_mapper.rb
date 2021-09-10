3e43de242ef4f6bb85cdd957a9600fb22ce0f784
Postmodern <postmodern.mod3@gmail.com>
2021-09-10 10:57:27 -0700

    Added documentation for the ArgumentValue.

--- lib/command_mapper/argument_value.rb
+++ lib/command_mapper/argument_value.rb
@@ -1,8 +1,20 @@
 module CommandMapper
   class ArgumentValue
 
+    # The argument value's optional format.
+    #
+    # @return [#call, nil]
     attr_reader :format
 
+    #
+    # Initializes the argument value.
+    #
+    # @param [#call] format
+    #   The optional format for the argument value.
+    #
+    # @param [Boolean] required
+    #   Specifies whether the argument is required or can be omitted.
+    #
     def initialize(format: nil, required: true, &block)
       @format   = format
       @required = required
@@ -10,14 +22,31 @@ module CommandMapper
       @formatter = block
     end
 
+    #
+    # Specifies whether the argument value is required.
+    #
+    # @return [Boolean]
+    #
     def required?
       @required
     end
 
+    #
+    # Specifies whether the argument value can be omitted.
+    #
+    # @return [Boolean]
+    #
     def optional?
       !@required
     end
 
+    #
+    # Formats the given value.
+    #
+    # @param [Object] value
+    #
+    # @return [String]
+    #
     def format_value(value)
       if @format then @format.call(value)
       else            value.to_s
