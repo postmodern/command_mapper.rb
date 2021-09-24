1c6a8886206d9c1e6675259223eafd859db866cd
Postmodern <postmodern.mod3@gmail.com>
2021-09-24 08:18:41 -0700

    Added Types::Num to represent numeric values.

---
+++ lib/command_mapper/types/num.rb
@@ -0,0 +1,36 @@
+module CommandMapper
+  module Types
+    #
+    # Represents a numeric value.
+    #
+    class Num < Value
+
+      #
+      # Validates a value.
+      #
+      # @param [String, Integer] value
+      #
+      # @return [true, (false, String)]
+      #
+      def validate(value)
+        valid, message = super(value)
+
+        unless valid
+          return valid, message
+        end
+
+        case value
+        when Integer
+          return true
+        else
+          if value.respond_to?(:=~) && value =~ /^\d+$/
+            return true
+          else
+            return false, "value must be numeric"
+          end
+        end
+      end
+
+    end
+  end
+end
