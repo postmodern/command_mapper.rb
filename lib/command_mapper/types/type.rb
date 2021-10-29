b7aaa6738240160114a680fc4a6f9bc8bf992564
Postmodern <postmodern.mod3@gmail.com>
2021-10-28 19:35:44 -0700

    Added documentation on how to define custom Types to the Type class.

--- lib/command_mapper/types/type.rb
+++ lib/command_mapper/types/type.rb
@@ -3,6 +3,53 @@ module CommandMapper
     #
     # The base type for all command-line argument types.
     #
+    # ## Custom Types
+    #
+    # Custom types can be defined by extending the {Type} class.
+    # 
+    #     class PortRange < CommandMapper::Types::Type
+    #     
+    #       def validate(value)
+    #         case value
+    #         when Integer
+    #           true
+    #         when Range
+    #           if value.begin.kind_of?(Integer)
+    #             true
+    #           else
+    #             [false, "port range can only contain Integers"]
+    #           end
+    #         else
+    #           [false, "port range must be an Integer or a Range of Integers"]
+    #         end
+    #       end
+    #     
+    #       def format(value)
+    #         case value
+    #         when Integer
+    #           "#{value}"
+    #         when Range
+    #           "#{value.begin}-#{value.end}"
+    #         end
+    #       end
+    #     
+    #     end
+    #
+    #
+    # The custom type can define the following methods:
+    #
+    # * `#initialize` - accepts additional configuration options.
+    # * `#validate` - accepts a value object and returns `true` (indicating the
+    #   value is valid) or `[false, message]` (indicating the value is invalid).
+    # * `#format` - accepts a validated value and returns a formatted String.
+    #
+    # Once defined, custom {Type} classes can be used with `option` or
+    # `argument` and passed in via the `type:` keyword argument.
+    #
+    #     option "--ports", value: {required: true, type: PortRange.new}
+    #
+    #     argument :ports, required: true, type: PortRange.new
+    #
     class Type
 
       #
