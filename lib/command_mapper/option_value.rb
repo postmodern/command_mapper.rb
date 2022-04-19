3f3b36dd0da5f96ff418026d35221ac512df4499
Postmodern <postmodern.mod3@gmail.com>
2022-04-19 10:59:20 -0700

    Added custom validation logic for when a non-required option is given `true`.

--- lib/command_mapper/option_value.rb
+++ lib/command_mapper/option_value.rb
@@ -7,6 +7,24 @@ module CommandMapper
   class OptionValue < Arg
 
     #
+    # Validates whether a given value is compatible with the option {#type}.
+    #
+    # @param [Object] value
+    #   The given value to validate.
+    #
+    # @return [true, (false, String)]
+    #   Returns true if the value is valid, or `false` and a validation error
+    #   message if the value is not compatible.
+    #
+    def validate(value)
+      if !required? && value == true
+        return true
+      else
+        super(value)
+      end
+    end
+
+    #
     # Formats a value using the options {#type}.
     #
     # @param [Object] value
--- spec/option_value_spec.rb
+++ spec/option_value_spec.rb
@@ -1,8 +1,36 @@
 require 'spec_helper'
 require 'command_mapper/option_value'
+require 'command_mapper/types/num'
 require 'command_mapper/types/list'
 
 describe CommandMapper::OptionValue do
+  describe "#validate" do
+    let(:type) { Types::Num.new }
+
+    subject { described_class.new(type: type) }
+
+    context "when the option value is not required" do
+      subject do
+        described_class.new(type: type, required: false)
+      end
+
+      context "and given a value of true" do
+        it "must return true" do
+          expect(subject.validate(true)).to be(true)
+        end
+      end
+    end
+
+    context "otherwise" do
+      it "must validate the value using #type.validate" do
+        expect(subject.validate('1234')).to be(true)
+        expect(subject.validate('foo')).to eq(
+          [false, "contains non-numeric characters (\"foo\")"]
+        )
+      end
+    end
+  end
+
   describe "#format" do
     let(:type) { Types::List.new }
 
