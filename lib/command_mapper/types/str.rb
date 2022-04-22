3d7249d263f4a11a4ec1d9d2dce6586f4b09aac8
Postmodern <postmodern.mod3@gmail.com>
2022-04-22 04:28:35 -0700

    Add a missing description to `Types::Str`.

--- lib/command_mapper/types/str.rb
+++ lib/command_mapper/types/str.rb
@@ -2,6 +2,9 @@ require 'command_mapper/types/type'
 
 module CommandMapper
   module Types
+    #
+    # Represents an arbitrary string value.
+    #
     class Str < Type
       #
       # Initializes the value.
