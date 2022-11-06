ec64f5087810421db1401f05c2209546d996fbeb
Postmodern <postmodern.mod3@gmail.com>
2022-11-06 09:27:45 -0800

    Normalize `Types::InputDir` specs.

--- spec/types/input_dir_spec.rb
+++ spec/types/input_dir_spec.rb
@@ -3,19 +3,21 @@ require 'command_mapper/types/input_dir'
 
 describe CommandMapper::Types::InputDir do
   describe "#validate" do
-    context "when given a valid file path" do
-      let(:value) { __FILE__ }
-
-      it "must return [false, 'directory does not exist (...)']" do
-        expect(subject.validate(value)).to eq([false, "directory does not exist (#{value.inspect})"])
-      end
-    end
-
     context "when given a valid directory path" do
       let(:value) { __dir__ }
 
       it "must return true" do
-        expect(subject.validate(value)).to eq(true)
+        expect(subject.validate(value)).to be(true)
+      end
+    end
+
+    context "when given a valid file path" do
+      let(:value) { __FILE__ }
+
+      it "must return [false, 'directory does not exist (...)']" do
+        expect(subject.validate(value)).to eq(
+          [false, "directory does not exist (#{value.inspect})"]
+        )
       end
     end
 
@@ -23,7 +25,9 @@ describe CommandMapper::Types::InputDir do
       let(:value) { "/path/does/not/exist" }
 
       it "must return [false, 'path does not exist (...)']" do
-        expect(subject.validate(value)).to eq([false, "path does not exist (#{value.inspect})"])
+        expect(subject.validate(value)).to eq(
+          [false, "path does not exist (#{value.inspect})"]
+        )
       end
     end
   end
